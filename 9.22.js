f48ddbf8e4a7031a92986ae6c0dd8934af293f99
xhx <xhx@example.com>
2016-09-22 10:30:38 +0800

    listonclick

--- 9.22.js
+++ 9.22.js
@@ -4,14 +4,24 @@
     // document.getElementById("picture1").src= "../nonew/images/aaa.jpg";
     var btn = document.getElementById("changepicture");
     var img = document.getElementById("picture1");
+    console.log(img);
     btn.onclick = function() {
-        if (img.src.match("xxx.jpg")) {
+      /* if (img.src.match("xxx.jpg")) {
             img.src = "../nonew/images/aaa.jpg";
         } else {
             img.src = "../nonew/images/xxx.jpg";
+        }*/
+
+         if (img.style.visibility=="visible") {
+            img.style.visibility = "hidden";
+        } else {
+            img.style.visibility = "visible";
         }
     }
-
+    var listclick = document.getElementById("list1");
+    listclick.onclick = function(){
+        console.log("a");
+    }
 
 
 
