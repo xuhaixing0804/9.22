4dddb05c82edce39dbce4e365cc143c53d1a3895
xhx <xhx@example.com>
2016-09-22 10:58:58 +0800

    mouse事件

--- 9.22.js
+++ 9.22.js
@@ -3,15 +3,15 @@
     document.getElementById("pp").innerHTML = "蓝色，加粗";
     // document.getElementById("picture1").src= "../nonew/images/aaa.jpg";
     var btn = document.getElementById("changepicture");
-    var img = document.getElementById("picture1");
-    console.log(img);
+    // var img = document.getElementById("picture1");
+    // console.log(img);
     btn.onclick = function() {
       /* if (img.src.match("xxx.jpg")) {
             img.src = "../nonew/images/aaa.jpg";
         } else {
             img.src = "../nonew/images/xxx.jpg";
         }*/
-
+/*
          if (img.style.visibility=="visible") {
             img.style.visibility = "hidden";
         } else {
@@ -21,10 +21,17 @@
     var listclick = document.getElementById("list1");
     listclick.onclick = function(){
         console.log("a");
+    }  */
+
+    
+}
+ var picture =  document.getElementById("picture1");
+     picture.src= "../nonew/images/aaa.jpg";
+     picture.onmouseover= function(){
+      console.log("鼠标放上");
+   }
+    picture.onmouseout = function(){
+      console.log("鼠标离开")
     }
-
-
-
-
 
 }(window))
